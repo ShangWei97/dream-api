6f915e32bc35bc926ebafd5812839ea5a6b77548
shangwei <shangwei@tuhu.cn>
2019-03-31 17:16:59 +0800

    添加跨域

--- src/main/java/com/dreamfield/dreamapi/configuration/CORSConfiguration.java
+++ src/main/java/com/dreamfield/dreamapi/configuration/CORSConfiguration.java
@@ -4,29 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 /**
- * 跨域配置
  * @author: shangwei
- * @date: 2019-03-26 16:20
+ * @date: 2019-03-31 16:44
  */
-@Configuration
-public class CORSConfiguration {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				//registry.addMapping("/api/**");
-				registry.addMapping("/**")
-						.allowedOrigins("/**")
-						.allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
-						.allowCredentials(false)
-						.maxAge(3600);
-			}
+@Configuration
+public class CORSConfiguration{
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+    	return new WebMvcConfigurer() {
+    		@Override
+		    public void addCorsMappings(CorsRegistry registry) {
+			    //设置访问的IP和端口，此代表所有都可以访问
+    			registry.addMapping("/**");
+    		}
 		};
 	}
+
 }
 
